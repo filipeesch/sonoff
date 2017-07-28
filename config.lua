573fc353ba439aea013cddf9ed752e895cca0363
filipeesch <filipeesch@hotmail.com>
2017-07-28 12:08:58 -0300

    initial commit

--- config.lua
+++ config.lua
@@ -1,10 +1,32 @@
-config = {}
+local config = {}
 config.wifi = {}
 
-config.wifi.read = function()
-    f = file.open("wifi.txt", "r");
+function config.wifi.read()
+    f = file.open("wifi.txt", "r")
     
-    if f then
-        wifiName = f:readline();
-    end
+    w = {}
+    w.name = "name"
+    w.password = "wireless"
+    return w
+
+-- if f then
+--     w.name = f:readline()
+--     w.password = f:readline()
+--     f:close()
+--     if w.name == nil or w.password == nil then
+--         return nil
+--     end
+--     return w
+-- end
+-- return nil
 end
+
+config.wifi.write = function(name, password)
+    f = file.open("wifi.txt", "w+")
+    
+    f:writeline(name)
+    f:writeline(password)
+    f:close()
+end
+
+return config
